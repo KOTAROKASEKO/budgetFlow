4964369a136eb75c97dfebaef42ff794a7e28c6b
KOTAROKASEKO <kase.ktr@gmail.com>
2025-05-03 19:21:38 +0800

    changed bottom nav

--- lib/View_BottomTab.dart
+++ lib/View_BottomTab.dart
@@ -17,67 +17,58 @@ class _BottomTabState extends State<BottomTab> {
     BuyList(),
   ];
 
-  void changeIndex() {
+  void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = _selectedIndex;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        _pages[_selectedIndex],
-        Positioned(
-            right: 100,
-            bottom: 11,
-            child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Container(
-                  width: 240,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 209, 209, 209),
-                        blurRadius: 3,
-                        spreadRadius: 3,
-                        offset: Offset(0, 2),
-                      )
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      GestureDetector(
-                        child: Image.asset(
-                          'assets/home.png',
-                          width: _selectedIndex == 0 ? 40 : 20,
-                          height: 40,
-                        ),
-                        onTap: () {
-                          _selectedIndex = 0;
-                          changeIndex();
-                        },
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          _selectedIndex = 1;
-                          changeIndex();
-                        },
-                        child: Icon(
-                          size: _selectedIndex == 1 ? 40 : 20,
-                          Icons.list,
-                          color: const Color.fromARGB(255, 0, 0, 0),
-                        ),
-                      )
-                    ],
-                  ),
-                )))
-      ],
-    ));
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30), // ← 両端だけ丸く
+            topRight: Radius.circular(30),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              offset: Offset(0, -2),
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            backgroundColor: Colors.white,
+            selectedItemColor: Colors.black,
+            unselectedItemColor: Colors.grey,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'List',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
