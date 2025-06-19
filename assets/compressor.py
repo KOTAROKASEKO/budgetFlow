2267ffc5ef5b23f34f7610cee39b942e5e26c239
KOTAROKASEKO <kase.ktr@gmail.com>
2025-06-19 14:27:34 +0800

    aaaa

--- assets/compressor.py
+++ assets/compressor.py
@@ -21,6 +21,6 @@ def optimize_png_pillow(input_path, output_path, colors=256, max_size=(1024, 768
         print(f"エラーが発生しました: {e}")
 
 # 使用例
-input_file = "aibackground.png"
-output_file = "aibackground.png"
+input_file = "background.png"
+output_file = "background.png"
 optimize_png_pillow(input_file, output_file, colors=128, max_size=(800, 600))
